affe7c356ab6822fa4a0752b448d61c15431cdc3
pilllea <lea.pillette@ensc.fr>
2016-01-07 15:11:28 +0100

    Améliorations
    Création de fonctions et ajout de commentaires

--- Body/CreateModel.cpp
+++ Body/CreateModel.cpp
@@ -18,22 +18,8 @@ using namespace OpenSim;
 using namespace SimTK;
 using namespace std;
 
-int main()
-{
-	//Creation of the model which will be composed of all the elements
-	Model osimModel;
-	osimModel.setName("bodyModel");
-	//Allow real time visualization of the model
-	//osimModel.setUseVisualizer(true);
-
-	// Get a reference to the model's ground body
-	OpenSim::Body& ground = osimModel.getGroundBody();
-
-	//Map associating body part name and the bones that are in this body part
-	std::map< string, vector<string> > bodyNames;
-	//Map associating the body part name and the suffix necessary to acces the .VTP bone file
-	std::map<string, string> suffix;
-
+//Map associating body part name and the bones that are in this body part
+void createBodyNamesMap(std::map< string, vector<string> > &bodyNames, std::map<string, string> &suffix) {
 	//Main
 	bodyNames["torso"] = { "hat_jaw", "hat_spine", "hat_skull", "hat_ribs_scap" };
 	bodyNames["pelvis"] = { "sacrum", "pelvis", "l_pelvis" };
@@ -65,61 +51,44 @@ int main()
 	bodyNames["radius_l"] = { "radius_lv" };
 	bodyNames["hand_l"] = { "pisiform", "lunate", "scaphoid", "triquetrum", "hamate", "capitate", "trapezoid", "trapezium", "metacarpal2", "index_proximal", "index_medial", "index_distal", "metacarpal3", "middle_proximal", "middle_medial", "middle_distal", "metacarpal4", "ring_proximal", "ring_medial", "ring_distal", "metacarpal5", "little_proximal", "little_medial", "little_distal", "metacarpal1", "thumb_proximal", "thumb_distal" };
 	suffix["hand_l"] = "_lsv";
+}
 
-	//Properties of the body part (can be skiped)
-	std::map< string, vector<double> > bodyProp;
+//Properties of the body part (can be skiped)
+void createBodyPropMap(std::map <string, vector<double> > &bodyProp) {
 	bodyProp["torso"] = { 26.82, 0.1 };
 	bodyProp["humerus_r"] = { 0, 0 };
 	bodyProp["lowerArm"] = { 20.0, 0.1 };
-	Vec3 center(0);
+}
 
-	std::string name, suf;
-	Inertia inertia;
-	double mass, length;
-	//Contains all the bodies accessible by their body names
-	std::map< string, OpenSim::Body* > bodies;
-	//Creates all the bodies (= body parts)
-	for (std::map< string, vector<string> >::const_iterator it = bodyNames.begin(); it != bodyNames.end(); ++it) {
-		//Initialize variables linked to the body
-		mass = (bodyProp.find(it->first) != bodyProp.end()) ? bodyProp[it->first][0] : 0.;
-		length = (bodyProp.find(it->first) != bodyProp.end()) ? bodyProp[it->first][1] : 0.;
-		inertia = mass*Inertia::brick(length, length, length);
-		suf = (suffix.find(it->first) != suffix.end()) ? suffix[it->first] : "";
-		// Create a new block body with specified properties
-		OpenSim::Body *b = new OpenSim::Body(it->first, mass, center, inertia);
-		// Associate the bones to the body
-		for (int i = 0; i < it->second.size(); i++) {
-			name = it->second[i] + suf + ".vtp";
-			b->addDisplayGeometry(name);
-		}
-		// Store the bodies in the map
-		bodies[it->first] = b;
-	}
-	
-	//Map associating of the joints properties
-	std::map< string, vector<string> > jointsNames;
-	jointsNames["back"] = { "pelvis", "torso" };
-	jointsNames["hip_r"] = { "pelvis", "femur_r" };
-	jointsNames["hip_l"] = { "pelvis", "femur_l" };
-	jointsNames["knee_r"] = { "femur_r", "tibia_r" };
-	jointsNames["knee_l"] = { "femur_l", "tibia_l" };
-	jointsNames["ankle_r"] = { "tibia_r","talus_r" };
-	jointsNames["ankle_l"] = { "tibia_l","talus_l" };
-	jointsNames["subtalar_r"] = { "talus_r", "calcn_r" };
-	jointsNames["subtalar_l"] = { "talus_l", "calcn_l" };
-	jointsNames["mtp_r"] = { "calcn_r", "toes_r" };
-	jointsNames["mtp_l"] = { "calcn_l", "toes_l" };
+//Map associating of the joints names and the name of the bodies they link
+void createJointNamesMap(std::map< string, vector<string> > &jointNames) {
+	jointNames["back"] = { "pelvis", "torso" };
 
-	jointsNames["acromial_r"] = { "torso", "humerus_r" };
-	jointsNames["acromial_l"] = { "torso", "humerus_l" };
-	jointsNames["elbow_r"] = { "humerus_r", "ulna_r" };
-	jointsNames["elbow_l"] = { "humerus_l", "ulna_l" };
-	jointsNames["radioulnar_r"] = { "ulna_r", "radius_r" };
-	jointsNames["radioulnar_l"] = { "ulna_l", "radius_l" };
-	jointsNames["radius_hand_r"] = { "radius_r", "hand_r" };
-	jointsNames["radius_hand_l"] = { "radius_l", "hand_l" };
+	//Legs
+	jointNames["hip_r"] = { "pelvis", "femur_r" };
+	jointNames["hip_l"] = { "pelvis", "femur_l" };
+	jointNames["knee_r"] = { "femur_r", "tibia_r" };
+	jointNames["knee_l"] = { "femur_l", "tibia_l" };
+	jointNames["ankle_r"] = { "tibia_r","talus_r" };
+	jointNames["ankle_l"] = { "tibia_l","talus_l" };
+	jointNames["subtalar_r"] = { "talus_r", "calcn_r" };
+	jointNames["subtalar_l"] = { "talus_l", "calcn_l" };
+	jointNames["mtp_r"] = { "calcn_r", "toes_r" };
+	jointNames["mtp_l"] = { "calcn_l", "toes_l" };
 
-	std::map< string, Vec3 > jointProp;
+	//Arms
+	jointNames["acromial_r"] = { "torso", "humerus_r" };
+	jointNames["acromial_l"] = { "torso", "humerus_l" };
+	jointNames["elbow_r"] = { "humerus_r", "ulna_r" };
+	jointNames["elbow_l"] = { "humerus_l", "ulna_l" };
+	jointNames["radioulnar_r"] = { "ulna_r", "radius_r" };
+	jointNames["radioulnar_l"] = { "ulna_l", "radius_l" };
+	jointNames["radius_hand_r"] = { "radius_r", "hand_r" };
+	jointNames["radius_hand_l"] = { "radius_l", "hand_l" };
+}
+
+//Properties of the joints (can be skiped)
+void createJointPropMap (std::map< string, Vec3 > &jointProp) {
 	jointProp["hip_r"] = Vec3(-0.0707, -0.0661, 0.0835);
 	jointProp["ankle_r"] = Vec3(0, -0.43, 0);
 	jointProp["subtalar_r"] = Vec3(-0.04877, -0.04195, 0.00792);
@@ -137,17 +106,78 @@ int main()
 	jointProp["elbow_r"] = Vec3(0.013144, -0.286273, 0.009595);
 	jointProp["radioulnar_r"] = Vec3(-0.006727, -0.013007, -0.026083);
 	jointProp["radius_hand_r"] = Vec3(-0.008797, -0.235841, -0.01361);
+}
 
-	//Creates all the joints
+int main()
+{
+	//Creation of the model which will be composed of all the elements
+	Model osimModel;
+	osimModel.setName("bodyModel");
+	//Allow real time visualization of the model
+	//osimModel.setUseVisualizer(true);
+
+	// Get a reference to the model's ground body
+	OpenSim::Body& ground = osimModel.getGroundBody();
+
+	//Map associating body part name and the bones that are in this body part
+	std::map< string, vector<string> > bodyNames;
+	
+	//Map associating the body part name and the suffix necessary to acces the .VTP bone file
+	std::map<string, string> suffix;
+
+	//Creates the bodyNames map and the suffix map
+	createBodyNamesMap(bodyNames, suffix);
+
+	//Properties of the body part (can be skiped)
+	std::map< string, vector<double> > bodyProp;
+	createBodyPropMap(bodyProp);
+
+	Vec3 center(0);
+	std::string name, suf;
+	Inertia inertia;
+	double mass, length;
+	//Contains all the bodies accessible by their body names
+	std::map< string, OpenSim::Body* > bodies;
+	//Creates all the bodies (= body parts)
+	for (std::map< string, vector<string> >::const_iterator it = bodyNames.begin(); it != bodyNames.end(); ++it) {
+		//Initialize variables linked to the body with the bodyProp map 
+		mass = (bodyProp.find(it->first) != bodyProp.end()) ? bodyProp[it->first][0] : 0.;
+		length = (bodyProp.find(it->first) != bodyProp.end()) ? bodyProp[it->first][1] : 0.;
+		inertia = mass*Inertia::brick(length, length, length);
+		suf = (suffix.find(it->first) != suffix.end()) ? suffix[it->first] : "";
+		// Create a new block body with specified properties
+		OpenSim::Body *b = new OpenSim::Body(it->first, mass, center, inertia);
+		// Associate the bones to the body
+		for (int i = 0; i < it->second.size(); i++) {
+			name = it->second[i] + suf + ".vtp";
+			b->addDisplayGeometry(name);
+		}
+		// Store the bodies in the map
+		bodies[it->first] = b;
+	}
+
+	//Map associating of the joints names and the name of the bodies they link
+	std::map< string, vector<string> > jointNames;
+	createJointNamesMap(jointNames);
+
+	//Properties of the joints (can be skiped)
+	std::map< string, Vec3 > jointProp;
+	createJointPropMap(jointProp);
+	
 	//Create a new pin joint between the ground and the pelvis
 	Vec3 locationInParent(0), orientationInParent(0), locationInBody(0), orientationInBody(0);
 	PinJoint *ground_pelvis = new PinJoint("ground_pelvis", ground, locationInParent, orientationInParent, *bodies["pelvis"], locationInBody, orientationInBody);
 
+	//Contains all the joints accessible by their joints names
 	std::map< string, OpenSim::FreeJoint* > joints;
 	double positionRange[2] = { -0.01, 0.01 };
-	for (std::map< string, vector<string> >::const_iterator it = jointsNames.begin(); it != jointsNames.end(); ++it) {
+	//Creates all the joints using the list of joint names contained in the map named jointNames
+	for (std::map< string, vector<string> >::const_iterator it = jointNames.begin(); it != jointNames.end(); ++it) {
+		//Get property of the joint if it exists
 		locationInParent = (jointProp.find(it->first) != jointProp.end()) ? jointProp[it->first] : Vec3(0);
+		//Create the new joint
 		FreeJoint *j = new FreeJoint(it->first, *bodies[it->second[0]], locationInParent, orientationInParent, *bodies[it->second[1]], locationInBody, orientationInBody);
+		// Store the bodies in the map
 		joints[it->first] = j;
 		// Get a reference to the coordinate set (6 degrees-of-freedom) between the block and ground bodies
 		CoordinateSet& jointCoordinateSetH = j->upd_CoordinateSet();
@@ -163,23 +193,14 @@ int main()
 		jointCoordinateSetH[4].setRange(positionRange);
 		jointCoordinateSetH[5].setRange(positionRange);
 	}
-	cout << "3" << endl;
+	
 	// Add every body to the model
 	for (std::map< string, OpenSim::Body* >::const_iterator it = bodies.begin(); it != bodies.end(); ++it) {
 		cout << bodies[it->first]->getName() << endl;
 		OpenSim::Body *b(it->second);
 		osimModel.addBody(b);
-		//cout << "hello3.5" << endl;
-	}
-	cout << "hello" << endl;
-	for (std::map< string, OpenSim::FreeJoint* >::const_iterator it = joints.begin(); it != joints.end(); ++it) {
-		cout << joints[it->first]->getName() << endl;
-
-		//osimModel.addBody((OpenSim::Body*)(it->second));
-		//cout << "hello3.5" << endl;
 	}
 
-	cout << "4" << endl;
 	// Define the acceleration of gravity
 	osimModel.setGravity(Vec3(0, -9.80665, 0));
 
